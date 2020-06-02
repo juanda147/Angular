ef2c2afa8689ce6fab2a5a4b85ebda18383adc97
Juan David Castaneda <juan.castaneda@salsamobi.com>
2020-06-02 16:14:35 -0500

    partial changes form recipes app

--- recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('A Test Recipe', 'This is simply a test','https://p0.pikrepo.com/preview/101/421/grilled-pork-belly-with-green-salad.jpg'),
+  new Recipe('A Test Recipe', 'This is simply a test','https://p0.pikrepo.com/preview/101/421/grilled-pork-belly-with-green-salad.jpg')];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
